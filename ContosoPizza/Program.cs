b5b118d95e875ea158b3444944853e67a15206de
MrSpiderPablo <pabloramosblanquez@gmail.com>
2023-12-14 14:34:07 +0100

    Tarea 4-7
    Finalizado

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,3 +1,4 @@
+using ContosoPizza.Data;
 using ContosoPizza.Services;
 // Additional using declarations
 
@@ -8,7 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add the PizzaContext
-
+builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
 // Add the PromotionsContext
 
 builder.Services.AddScoped<PizzaService>();
@@ -27,6 +28,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 // Add the CreateDbIfNotExists method call
+app.CreateDbIfNotExists();
 
 app.MapGet("/", () => @"Contoso Pizza management API. Navigate to /swagger to open the Swagger test UI.");
 
